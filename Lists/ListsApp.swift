bc76fde2c730d827a57c4eb2009f5c3b2348ac34
Mark Nair <marknair@me.com>
2023-11-14 14:39:36 -0600

    Initial Commit

---
+++ Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by Mark Nair on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
